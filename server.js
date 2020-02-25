65bc38d6be1f6100da201971ad76533638090892
Atila Fassina <atila@fassina.eu>
2020-02-26 00:14:33 +0100

    ✨ get routing to work with default language

--- server.js
+++ server.js
@@ -1,33 +1,63 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+let { languages } = require('./package.json')
+let { supportedLanguages, defaultLanguage } = languages
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+let { createServer } = require('http')
+let { parse } = require('url')
+let next = require('next')
+
+let dev = process.env.NODE_ENV !== 'production'
+let app = next({ dev })
+let handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+    let parsedUrl = parse(req.url, true)
+    let { pathname, query } = parsedUrl
 
-    if (pathname === '/en') {
+    if (pathname.startsWith(`/${defaultLanguage}`)) {
       res.writeHead(301, {
-        Location: '/'
+        // the fallback is for the root (`/en`)
+        Location: pathname.substring(3) || '/'
       })
 
       res.end()
     }
 
-    if (pathname === '/') {
-      app.render(req, res, '/en', query)
-    } else if (pathname === '/pt') {
-      app.render(req, res, '/pt', query)
-    } else {
-      handle(req, res, parsedUrl)
+    let REGEXpattern = supportedLanguages.reduce(
+      (pattern, lang, index) =>
+        index + 1 < supportedLanguages.length
+          ? `${pattern}(\/${lang}\/?)|`
+          : `${pattern}(\/${lang}\/?)`,
+      ''
+    )
+
+    let langREGEX = new RegExp(`^${REGEXpattern}`)
+    let matchedLang = pathname.match(langREGEX)
+
+    if (matchedLang === null) {
+      app.render(req, res, `/en${pathname.length > 1 ? pathname : ''}`, query)
+      return
+    }
+
+    switch (matchedLang[0]) {
+      case `/${defaultLanguage}`:
+      case `/${defaultLanguage}/`:
+        handle(req, res, parsedUrl)
+        break
+
+      default:
+        app.render(
+          req,
+          res,
+          pathname.endsWith('/')
+            ? pathname.substring(0, pathname.length - 1)
+            : pathname,
+          query
+        )
+        break
     }
   }).listen(3000, err => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('\n❯ Ready on http://localhost:3000\n')
   })
 })
