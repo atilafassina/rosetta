c46a22cc85e927b6bea36eb1e054aea32d06181b
Ademílson F. Tonato <ademilsonft@outlook.com>
2020-02-26 10:02:56 +0000

    feat: adding new examples to navigate between pages and langs

--- pages/en/index.js
+++ pages/en/index.js
@@ -34,6 +34,41 @@ const Home = () => (
         <code css={code}>pages/{'{{ lang }}'}/index.js</code>
       </p>
 
+      <p css={description}>
+        You can navigate between pages and langs
+      </p>
+
+      <div css={grid}>
+        <a
+          href="/"
+          css={card}
+        >
+          <h3><code css={code}>/en/index</code></h3>
+          <p>You're here!</p>
+        </a>
+        <a
+          href="/en/page2"
+          css={card}
+        >
+          <h3><code css={code}>/en/page2</code></h3>
+          <p>Another <strong>EN</strong> page.</p>
+        </a>
+        <a
+          href="/eo/"
+          css={card}
+        >
+          <h3><code css={code}>/eo/index</code></h3>
+          <p>Go to <strong>EO</strong> index page</p>
+        </a>
+        <a
+          href="/eo/page2"
+          css={card}
+        >
+          <h3><code css={code}>/eo/page2</code></h3>
+          <p>Another <strong>EO</strong> page.</p>
+        </a>
+      </div>
+
       <div css={grid}>
         <a
           href="https://nextjs.org/docs"
